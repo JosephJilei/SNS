c84b71051dc36f9cf4715d8fcda2503655265ba9
JosephJilei <cxxyjilei@126.com>
2014-03-21 15:15:46 +0800

    modified-commit
    Signed-off-by: JosephJilei <cxxyjilei@126.com>

--- sns.js
+++ sns.js
@@ -10,31 +10,31 @@ var  SNS ={
 		var shareurl,sharetitle,sharepic,finalurl="";
 		switch(type){
 			case 1:
-				shareurl="http://service.weibo.com/share/share.php?url="+encodeURIComponent(this.defaultConfig.sharesharedomain);
+				shareurl="http://service.weibo.com/share/share.php?url="+encodeURIComponent(this.defaultConfig.sharedomain);
 				sharetitle="&title="+encodeURIComponent(this.defaultConfig.description);
 				sharepic="&pic="+encodeURIComponent(this.defaultConfig.sharedomain+this.defaultConfig.pic);
 				finalurl=shareurl+sharetitle+sharepic;
 			break;
 			case 2:
-				shareurl="http://share.v.t.qq.com/index.php?c=share&a=index&url="+encodeURIComponent(this.defaultConfig.sharesharedomain);
+				shareurl="http://share.v.t.qq.com/index.php?c=share&a=index&url="+encodeURIComponent(this.defaultConfig.sharedomain);
 				sharetitle="&title="+encodeURIComponent(this.defaultConfig.description);
 				sharepic="&pic="+encodeURIComponent(this.defaultConfig.sharedomain+this.defaultConfig.pic);
 				finalurl=shareurl+sharetitle+sharepic;
 			break;
 			case 3:
-				shareurl="http://widget.renren.com/dialog/share?resourceUrl="+encodeURIComponent(this.defaultConfig.sharesharedomain);
+				shareurl="http://widget.renren.com/dialog/share?resourceUrl="+encodeURIComponent(this.defaultConfig.sharedomain);
 				sharetitle="&title="+encodeURIComponent(this.title)+"&description="+encodeURIComponent(this.defaultConfig.description+""+this.defaultConfig.sharedomain);
 				sharepic="&pic="+encodeURIComponent(this.defaultConfig.sharedomain+this.defaultConfig.pic);
 				finalurl=shareurl+sharetitle+sharepic;
 			break;
 			case 4:
-				shareurl="http://www.kaixin001.com/rest/records.php?url="+encodeURIComponent(this.defaultConfig.sharesharedomain);
+				shareurl="http://www.kaixin001.com/rest/records.php?url="+encodeURIComponent(this.defaultConfig.sharedomain);
 				sharetitle="&content="+encodeURIComponent(this.defaultConfig.description);
 				sharepic="&pic="+encodeURIComponent(this.defaultConfig.sharedomain+this.defaultConfig.pic);
 				finalurl=shareurl+sharetitle+sharepic+"&starid=0&aid=0&style=11&stime=&sig=";
 			break;
 			case 5:
-				shareurl="http://shuo.douban.com/!service/share?href="+encodeURIComponent(this.defaultConfig.sharesharedomain);
+				shareurl="http://shuo.douban.com/!service/share?href="+encodeURIComponent(this.defaultConfig.sharedomain);
 				sharetitle="&name="+encodeURIComponent(this.defaultConfig.title)+"&text="+encodeURIComponent(this.defaultConfig.description);
 				sharepic="&image="+encodeURIComponent(this.defaultConfig.sharedomain+this.defaultConfig.pic);
 				finalurl=shareurl+sharetitle+sharepic+"&starid=0&aid=0&style=11&stime=&sig=";
